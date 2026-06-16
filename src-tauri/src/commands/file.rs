25bab95d3837ce60224b0f51b7381687b299c3d3
Daniil <dga03@mail.ru>
2026-06-17 00:39:03 +0600

    Start ImportPairsDialog implementation

---
+++ src-tauri/src/commands/file.rs
@@ -0,0 +1,6 @@
+use std::fs;
+
+#[tauri::command]
+pub fn read_text_file(path: String) -> Result<String, String> {
+    fs::read_to_string(&path).map_err(|e| format!("Failed to read file: {e}"))
+}
--- src/pages/DictionariesPage/components/EditorView/ImportPairsDialog.tsx
+++ src/pages/DictionariesPage/components/EditorView/ImportPairsDialog.tsx
@@ -2,16 +2,25 @@ import { useState, type ReactNode } from "react"
 import {
   Dialog,
   DialogContent,
-  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 interface ImportPairsDialogProps {
   children: ReactNode
 }
 
-export default ({ children }: ImportPairsDialogProps) => {
+const ImportPairsDialog = ({ children }: ImportPairsDialogProps) => {
   const [open, setOpen] = useState(false);
 
   const handleOpenChange = (open: boolean) => {
@@ -23,14 +32,27 @@ export default ({ children }: ImportPairsDialogProps) => {
       <DialogTrigger asChild>
         {children}
       </DialogTrigger>
-      <DialogContent aria-description={undefined} className="flex gap-0 flex-col max-h-80 min-w-60 max-w-120 sm:max-w-120 h-[calc(100dvh-2rem)] w-[calc(100vw-2rem)]">
-        <DialogHeader className="h-fit">
-          <DialogTitle>Import pairs</DialogTitle>
-        </DialogHeader>
-        <div className="">
+      <DialogContent aria-description={undefined} className="p-2 flex gap-1 flex-row max-h-80 max-w-120 sm:max-w-120 h-[calc(100dvh-2rem)] w-[calc(100vw-2rem)]">
+        <Textarea className="flex-1" />
+        <div className="flex gap-2 flex-col flex-1 mt-2 ml-2">
+          <DialogTitle className="mb-1">Import pairs</DialogTitle>
+          <div className="grid w-full max-w-xs items-center gap-1.5">
+            <Label htmlFor="separator-select">Separator</Label>
+            <Select defaultValue="-">
+              <SelectTrigger size="sm" id="separator-select">
+                <SelectValue placeholder="Separator" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="-">Dash (-)</SelectItem>
+                <SelectItem value=":">Colon (:)</SelectItem>
+                <SelectItem value="::">Double colon (::)</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
   )
 }
 
+export default ImportPairsDialog;
